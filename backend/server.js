6675e9481c261afcc6eb378bf9c1e393e6af1c04
Pratik Patil <pratikpatil2069@gmail.com>
2026-03-12 18:27:19 +0530

    remove the mvc architecture for better understanding the flow of project

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,143 @@
 import express from "express";
 import cors from "cors";
-import connectDb from "./config/db.js";
-import studentRoutes from "./routes/studentRoutes.js";
-
+import mongoose from "mongoose";
 
 const app=express();
-connectDb();
 
 //Middle wares.
 app.use(cors());
 app.use(express.json())
 
 
-app.use("/api/students", studentRoutes);
+// Connect to MongoDB
+const connectDb=async()=>{
+    try{
+        await mongoose.connect("mongodb://127.0.0.1:27017/student_db");
+        console.log("database connection successful!");
+    }catch(error){
+        console.error("connection failed",error.message);
+        process.exit(1);
+    }
+};
+connectDb();
+
+// Student schema & model
+const Schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique: true,
+      match: [/.+@.+\..+/, "Please enter a valid email"]
+    },
+    age: {
+      type: Number,
+      required: [true, "Age is required"],
+      min: [1, "Age must be at least 1"],
+      max: [120, "Age must be less than 120"]
+    },
+    course: {
+      type: String
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+const studentSchema = mongoose.model("studentSchema", Schema);
+
+// 1.Add Students route.
+app.post("/api/students/add", async (req, res) => {
+  try {
+   const student=await studentSchema.create(req.body);
+    res.status(201).json({ 
+        message: "Student added", 
+        data: student 
+    });
+  } catch (err) {
+    res.status(500).json({ 
+        error: err.message 
+    });
+  }
+});
+
+// 2. Get All Students route.
+ app.get("/api/students" ,async(req,res)=>{
+  try{
+    const page=parseInt(req.query.page) || 1;
+    const limit=parseInt(req.query.limit) || 5;
+    const skip=(page-1)*limit;
+    const search=req.query.search || "";
+    const sortField =req.query.sort || "name";
+    const sortOrder=req.query.order==="desc"?-1:1;
+
+    const total= await studentSchema.countDocuments({name:{$regex:search,$options:"i"}});
+    const students=await studentSchema.find({name:{$regex:search,$options:"i"}}).skip(skip).limit(limit).sort({[sortField]:sortOrder});
+
+    res.status(200).json({
+      success:true,
+      totalStudents:total,
+      currentPage:page,
+      totalPages:Math.ceil(total/limit),
+      data:students
+    });
+  } catch(error){
+    res.status(500).json({
+      success:false,
+      message:error.message
+    });
+  }
+});
+
+// 3.delete student route.
+app.delete("/api/students/delete/:id",async(req,res)=>{
+  try{
+    const student=await studentSchema.findByIdAndDelete(req.params.id);
+     if (!student) {
+      return res.status(404).json({
+        success: false,
+        message: "Student not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Student deleted successfully"
+    });
+  }catch(error){
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  } 
+});
+
+// update Student route.
+app.put("/api/students/update/:id", async(req,res)=>{
+  try{
+     const student= await studentSchema.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true});
+         if (!student) {
+          return res.status(404).json({
+            success: false,
+            message: "Student not found",
+          });
+        }
+         res.status(200).json({
+          success: true,
+          data: student,
+        });
+  }catch(error){
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+})
 
 
 //test routes.
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,149 @@
-import { useEffect,useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 
-function App(){
+function App() {
 
-  const [students,setStudents]=useState([]);
-  useEffect(()=>{
-    axios.get("http://localhost:5000/api/students")
-    .then((res)=>{setStudents(res.data.data)})
-    .catch((err)=>{console.log(err)});
-  },[]);
+  const [students, setStudents] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const [editId ,setEditId]=useState(null);
+
+  // Fetch students from backend
+  const fetchStudents = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/api/students");
+      setStudents(res.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Load students when page loads
+  useEffect(() => {
+    fetchStudents();
+  }, []);
+
+  // Add student
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const studentData = {
+      name,
+      email,
+      age: Number(age)
+    };
+    if(editId){
+      try{
+        await axios.put(`http://localhost:5000/api/students/update/${editId}`,studentData);
+      }catch(error){
+        console.log(error);
+      }
+    }else{
+      try {
+       await axios.post("http://localhost:5000/api/students/add",studentData);
+      }catch (error) {
+        console.log(error);
+      }
+    }
+    fetchStudents(); // refresh list
+
+      setName("");
+      setEmail("");
+      setAge("");
+      setEditId(null);
+  };
+
+  // Delete student
+  const deleteStudent = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/api/students/delete/${id}`);
+      fetchStudents();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //update student
+  
+  const updateStudent= (student)=>{
+        setName(student.name);
+        setEmail(student.email);
+        setAge(student.age);
+        setEditId(student._id);
+  };
+    
+  
+
   return (
-    <div>
-      <h1>Student-Management-App</h1>
-      {(students.length===0)
-      ?(
-      <p>No students found</p>
-    ):(
-        students.map((student) => (
-          <div key={student._id}>
-            <p>Name: {student.name}</p>
-            <p>Email: {student.email}</p>
-            <p>Age: {student.age}</p>
-            <hr />
-          </div>
-        ))
-      )}
+    <div style={{ padding: "30px" }}>
+
+      <h1>Student Management</h1>
+
+      {/* Add Student Form */}
+
+      <form onSubmit={handleSubmit}>
+
+        <h2>Add Student</h2>
+
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+          type="number"
+          placeholder="Age"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+
+        <br /><br />
+
+        <button type="submit">{editId?"Update Student":"Add Student"}</button>
+
+      </form>
+
+      <hr />
+
+      {/* Student List */}
+
+      <h2>Student List</h2>
+
+      {students.map((student) => (
+        <div key={student._id}>
+
+          <p>
+            {student.name} | {student.email} | {student.age}
+          </p>
+
+          <button onClick={() => deleteStudent(student._id)}>
+            Delete
+          </button>
+
+          <button  onClick={() => updateStudent(student)}>
+            Edit
+          </button>
+
+          <hr />
+
+        </div>
+      ))}
+
     </div>
   );
 }
